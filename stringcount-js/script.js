d66679bbd430e4ba04dc0177591faf8c65330e7f
Khalid <khalid.mohamed@powercoders.org>
2024-04-23 11:29:56 +0200

    added condition

--- stringcount-js/script.js
+++ stringcount-js/script.js
@@ -1,3 +1,8 @@
-let userWord=prompt('Enter a word');
-let wordLength=userWord.length;
-window.alert("The word " +userWord + "has the length of " +wordLength);
+let userWord = prompt("Enter a word");
+let wordLength = userWord.length;
+
+if (userWord === "" || !userWord == "") {
+  window.alert("You have not entered a word.");
+} else {
+  window.alert("The word " + userWord + "has the length of " + wordLength);
+}
