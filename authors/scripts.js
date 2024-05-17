c5b3eae30abfdbc43aa03ef8e6003e0597033e11
Khalid <khalid.mohamed@powercoders.org>
2024-05-17 11:47:00 +0200

    .

---
+++ authors/scripts.js
@@ -0,0 +1,62 @@
+let authors = [
+    {
+        firstName: 'Andreas',
+        lastName: 'Neeser'
+    },
+    {
+        firstName: 'Anna',
+        lastName: 'Ruchat'
+    },
+    {
+        firstName: 'Arno',
+        lastName: 'Camenisch'
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Schibli'
+    },
+    {
+        firstName: 'Demian',
+        lastName: 'Leinhard'
+    },
+    {
+        firstName: 'Flurina',
+        lastName: 'Bader'
+    },
+    {
+        firstName: 'Franco',
+        lastName: 'Supino'
+    },
+    {
+        firstName: 'Lukas',
+        lastName: 'Hartmann'
+    },
+    {
+        firstName: 'Marius',
+        lastName: 'Popescu'
+    },
+    {
+        firstName: 'Reto',
+        lastName: 'Haenny'
+    },
+    {
+        firstName: 'Sandra',
+        lastName: 'Kuenzi'
+    },
+    {
+        firstName: 'Simon',
+        lastName: 'Libsig'
+    }
+];
+var list = document.getElementById("myList1");
+
+authors.forEach(function (author) {
+  var listItem = document.createElement("li");
+
+  listItem.innerHTML = `
+  <img src="img/tile_${author.firstName.toLowerCase()}_${author.lastName.toLowerCase()}.jpg"/>
+  <p>${author.firstName} ${author.lastName}</p>
+`;
+
+  list.appendChild(listItem);
+});
