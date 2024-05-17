5e59e377068290dfc6776895f294806458a5a89c
Khalid <khalid.mohamed@powercoders.org>
2024-05-17 10:38:53 +0200

    .

--- age-input-js/script.js
+++ age-input-js/script.js
@@ -17,3 +17,39 @@ switch (userAge) {
     default:
         break;
 }
+
+
+const weekdaysArray = [1, 5, 6, 3, 9];
+
+function weekNumbersToWords(array) {
+    let weekNames = [];
+    for (let i = 0; i < array.length; i++) {
+        switch (array[i]) {
+            case 1:
+                weekNames.push("Monday");
+                break;
+            case 2:
+                weekNames.push("Tuesday");
+                break;
+            case 3:
+                weekNames.push("Wednesday");
+                break;
+            case 4:
+                weekNames.push("Thursday");
+                break;
+            case 5:
+                weekNames.push("Friday");
+                break;
+            case 6:
+                weekNames.push("Saturday");
+                break;
+            case 0:
+                weekNames.push("Sunday");
+                break;
+            default:
+        }
+    }
+    return weekNames;
+}
+
+console.log(weekNumbersToWords(weekdaysArray));
